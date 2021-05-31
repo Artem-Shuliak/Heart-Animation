9bd1e8ca1b9bfa3df0f432b62c3f46753b288578
Artem Shuliak <artemshuliak@gmail.com>
2021-05-31 23:28:39 +0300

    initial commit

---
+++ Heart Animation/HeartAnimation .swift
@@ -0,0 +1,66 @@
+//
+//  Extension .swift
+//  Heart Animation
+//
+//  Created by Artem Chouliak on 4/3/21.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func addHeartAnimation() {
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
+    }
+    
+    @objc func handleTap(recognizer: UITapGestureRecognizer) {
+        (0...10).forEach { (_) in
+            let translation = recognizer.location(in: view)
+            createAnimation(translation: translation, duration: 2)
+        }
+    }
+    
+    func createAnimation(translation: CGPoint, duration: CFTimeInterval) {
+        // params
+        let duration = duration + Double.random(in: 0...1) * 1
+        
+        // Create Image
+        let imageView = UIImageView(image: UIImage(systemName: "heart.fill"))
+        imageView.tintColor = Bool.random() ? .red : .systemPink
+        
+        let dimension = 10 + Double.random(in: 0...1) * 50
+        imageView.frame = CGRect(x: 0, y: 0, width: dimension, height: dimension)
+        
+        // Animate Image
+        let animation = CAKeyframeAnimation(keyPath: "position")
+        animation.path = customPath(position: translation).cgPath
+        animation.duration = duration
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        animation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        
+        imageView.layer.add(animation, forKey: nil)
+        view.addSubview(imageView)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+            imageView.removeFromSuperview()
+        }
+    }
+        
+    
+    func customPath(position: CGPoint) -> UIBezierPath {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: position.x, y: position.y))
+        
+        let endPoint = CGPoint(x: position.x, y: 0)
+        
+        let randomXShift = 50 + Double.random(in: 0...1) * 400
+        let randomYShift = (100 + Double.random(in: 0...1) * 100)
+        
+        let cp1 = CGPoint(x: position.x + CGFloat(randomXShift), y: position.y - CGFloat(randomYShift))
+        let cp2 = CGPoint(x: position.x - CGFloat(randomXShift), y: position.y - CGFloat(randomYShift + 100))
+        
+        path.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
+        return path
+    }
+}
